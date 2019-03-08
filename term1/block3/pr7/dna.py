367a8a8c4bf0b2ab56bdbb6704789e33de55bd10
Апрель <shurshu.maria@yandex.ru>
2019-03-09 01:07:48 +0300

    4 CICLES

--- term1/block3/pr7/dna.py
+++ term1/block3/pr7/dna.py
@@ -3,13 +3,16 @@ A=0
 T=0
 G=0
 C=0
-for i,j,k,l in a:
+for i in a:
     if i=='A':
         A=A+1
+for j in a:
     if j=='T':
         T=T+1
+for k in a:
     if k=='G':
         G=G+1
+for l in a:
     if l=='C':
         C=C+1
 print(A+" "+T+" "+G+" "+C)
